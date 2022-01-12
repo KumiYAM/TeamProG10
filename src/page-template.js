608eb2df0de1f06070316f5d7e7c96cc3749f3dc
kumikoyamagata <sun.kumiko@protonmail.com>
2022-01-12 02:24:30 -0600

    modified page-template.js

--- src/page-template.js
+++ src/page-template.js
@@ -1,19 +1,29 @@
-module.exports = templateData => {
-  console.log(templateData);
+ module.exports = templateData => {
+   console.log(templateData);
 
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>TeamProG10 Team</title>
-   </head>
-   <body> 
-    <h1>${templateData.name}</h1>
-    <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
-   </body>
-    </html>   
-     `;
-};
+     return `
+     <!DOCTYPE html>
+     <html lang="en">
+    <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <title>TeamProG10 Team</title>
+
+       <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+       <header>
+         <div class="container flex-row justify-space-between align-center py-3">
+           <nav class="flex-row"> 
+            <h1>${templateData.name}</h1>
+             <h2>
+               <a href="https://github.com/${templateData.github}">Github</a>
+             </h2>
+           </nav>
+         </div>
+       </header>
+    </body>/   </html>   
+      `;
+ };
